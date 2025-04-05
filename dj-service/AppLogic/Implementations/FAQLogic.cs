1b105a9d08e0d5ac22891a6baf036a1e6e3f070e
Oakley Dye <70559682+oakleydye@users.noreply.github.com>
2025-04-04 23:29:36 -0600

    Update FAQLogic.cs

--- dj-service/AppLogic/Implementations/FAQLogic.cs
+++ dj-service/AppLogic/Implementations/FAQLogic.cs
@@ -1,9 +1,11 @@
-﻿namespace dj_service;
+using System.Linq;
+
+namespace dj_service;
 
 public class FAQLogic(IFAQAccess fAQAccess) : IFAQLogic
 {
     private readonly IFAQAccess _fAQAccess = fAQAccess;
-    public async Task<IEnumerable<FAQ>> GetFAQs() => await _fAQAccess.GetFAQs();
+    public async Task<IEnumerable<FAQ>> GetFAQs() => await _fAQAccess.GetFAQs().OrderBy(x => x.Id);
     public async Task<FAQ> GetFAQ(int id) => await _fAQAccess.GetFAQ(id);
     public async Task<FAQ> AddFAQ(FAQ faq) => await _fAQAccess.AddFAQ(faq);
     public async Task<FAQ> UpdateFAQ(FAQ faq) => await _fAQAccess.UpdateFAQ(faq);
