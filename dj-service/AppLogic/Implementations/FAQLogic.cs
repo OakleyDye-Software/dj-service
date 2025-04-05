70b267c6edb30e717633951afe0c6d4b59eca677
Oakley Dye <oakley.dye@gmail.com>
2025-04-04 23:33:01 -0600

    fixing build error

--- dj-service/AppLogic/Implementations/FAQLogic.cs
+++ dj-service/AppLogic/Implementations/FAQLogic.cs
@@ -5,7 +5,7 @@ namespace dj_service;
 public class FAQLogic(IFAQAccess fAQAccess) : IFAQLogic
 {
     private readonly IFAQAccess _fAQAccess = fAQAccess;
-    public async Task<IEnumerable<FAQ>> GetFAQs() => await _fAQAccess.GetFAQs().OrderBy(x => x.Id);
+    public async Task<IEnumerable<FAQ>> GetFAQs() => (await _fAQAccess.GetFAQs()).OrderBy(x => x.Id);
     public async Task<FAQ> GetFAQ(int id) => await _fAQAccess.GetFAQ(id);
     public async Task<FAQ> AddFAQ(FAQ faq) => await _fAQAccess.AddFAQ(faq);
     public async Task<FAQ> UpdateFAQ(FAQ faq) => await _fAQAccess.UpdateFAQ(faq);
